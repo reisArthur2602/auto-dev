565abedfdc66f5f75b0ac12e4fc965edf2dc8335
@dev.arthur <contato.arthurreis222@gmail.com>
2024-08-02 14:07:45 -0300

    feat: add page login inicial

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from '../App';
-import { Home } from '../pages';
+import { Home, Login } from '../pages';
 
 export default createBrowserRouter([
-  { element: <App />, children: [{ path: '/', element: <Home /> }] },
+  {
+    element: <App />,
+    children: [
+      { path: '/', element: <Login /> },
+      { path: 'home', element: <Home /> },
+    ],
+  },
 ]);
