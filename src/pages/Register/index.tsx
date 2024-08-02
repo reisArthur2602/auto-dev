da5bafda720a6aa9a17a13b9ff447032c3ca6957
@dev.arthur <contato.arthurreis222@gmail.com>
2024-08-02 14:39:52 -0300

    feat: add page register inicial

---
+++ src/pages/Register/index.tsx
@@ -0,0 +1,10 @@
+
+import { Container } from '../../components';
+
+export const Register = () => {
+  return (
+    <Container classname="flex flex-1 items-center justify-center">
+     Register
+    </Container>
+  );
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from '../App';
-import { Home, Login } from '../pages';
+import { Home, Login, Register } from '../pages';
 
 export default createBrowserRouter([
   {
     element: <App />,
     children: [
       { path: '/', element: <Login /> },
+      { path: '/register', element: <Register /> },
       { path: 'home', element: <Home /> },
     ],
   },
