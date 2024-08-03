d85ebbf6cb6244efccd944325e8368c100d0035e
@dev.arthur <contato.arthurreis222@gmail.com>
2024-08-03 09:19:10 -0300

    feat: add title home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,7 +4,9 @@ export const Home = () => {
   return (
     <Container classname="py-10 px-4 flex items-center flex-col gap-10">
       <SearchBar />
-      ...
+      <h1 className="text-center font-bold text-neutral-950 text-[2rem]">
+        Quer comprar ou vender um carro sem sair de casa?
+      </h1>
     </Container>
   );
 };
