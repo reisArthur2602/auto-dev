b9047db7f6595ed2e0b88ac363db100f0a8995cc
@dev.arthur <contato.arthurreis222@gmail.com>
2024-08-03 09:37:19 -0300

    feat: listar carros

--- src/dtos/car.ts
+++ src/dtos/car.ts
@@ -1,3 +1,22 @@
+export type CarsData = {
+  id: string;
+  name: string;
+  year: string;
+  uid: string;
+  price: string;
+  city: string;
+  km: string;
+  images: CarImageProps[];
+  owner: string;
+  created: Date;
+  description: string;
+};
+
+export type CarImageProps = {
+  name: string;
+  uid: string;
+  url: string;
+};
 
 export type Car = {
   name: string;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,40 @@
+import { useEffect, useState } from 'react';
 import { Container, SearchBar } from '../../components';
+import { collection, getDocs, orderBy, query } from 'firebase/firestore';
+import { db } from '../../services';
+import { CarsData } from '../../dtos/car';
+
+type CardCardProps = Omit<CarsData, 'owner' | 'description' | 'created'>;
 
 export const Home = () => {
+  const [cars, setCars] = useState<CardCardProps[]>([]);
+
+  useEffect(() => {
+    (() => {
+      const carsRef = collection(db, 'cars');
+      const queryRef = query(carsRef, orderBy('created', 'desc'));
+
+      getDocs(queryRef).then((snapshot) => {
+        let CARS = [] as CardCardProps[];
+
+        snapshot.forEach((doc) =>
+          CARS.push({
+            id: doc.id,
+            name: doc.data().name,
+            year: doc.data().year,
+            km: doc.data().km,
+            city: doc.data().city,
+            price: doc.data().price,
+            images: doc.data().images,
+            uid: doc.data().uid,
+          })
+        );
+        setCars(CARS);
+      });
+    })();
+  }, []);
+
+
   return (
     <Container classname="py-10 px-4 flex items-center flex-col gap-10">
       <SearchBar />
