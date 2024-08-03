0eb2575356a6dce976a8b23744170c4b381501b5
@dev.arthur <contato.arthurreis222@gmail.com>
2024-08-03 19:07:35 -0300

    feat: listar carros por id

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,7 +1,39 @@
-
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { CarsData } from '../../dtos/car';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../../services';
 
 export const Details = () => {
-  return (
-    <div>Details</div>
-  )
-}
+  const { id } = useParams();
+
+  const [car, setCar] = useState<CarsData>();
+
+  useEffect(() => {
+    (async () => {
+      if (!id) return;
+
+      const docRef = doc(db, 'cars', id);
+
+      getDoc(docRef).then((snapshot) => {
+        setCar({
+          id: snapshot.id,
+          name: snapshot.data()?.name,
+          year: snapshot.data()?.year,
+          city: snapshot.data()?.city,
+          model: snapshot.data()?.model,
+          uid: snapshot.data()?.uid,
+          description: snapshot.data()?.description,
+          created: snapshot.data()?.created,
+          phone: snapshot.data()?.whatsapp,
+          price: snapshot.data()?.price,
+          km: snapshot.data()?.km,
+          owner: snapshot.data()?.owner,
+          images: snapshot.data()?.images,
+        });
+      });
+    })();
+  }, [id]);
+
+  return <div>Details</div>;
+};
