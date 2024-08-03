9b4ccc013498b07a164d4d60abab426b213572a2
@dev.arthur <contato.arthurreis222@gmail.com>
2024-08-03 11:00:30 -0300

    feat: add page dashboard inicial

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { Container, NavBar } from '../../components';
+
+export const Dashboard = () => {
+  return (
+    <Container classname="py-10 px-4">
+      <NavBar />
+      ...
+    </Container>
+  );
+};
--- src/routes/auth.tsx
+++ src/routes/auth.tsx
@@ -1,11 +1,12 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { Home, New } from '../pages';
+import { Dashboard, Home, New } from '../pages';
 
 export default () => {
   return (
     <Routes>
       <Route path="/home" element={<Home />} />
       <Route path="/new" element={<New />} />
+      <Route path="/dashboard" element={<Dashboard />} />
       <Route path="*" element={<Navigate to="/home" />} />
     </Routes>
   );
