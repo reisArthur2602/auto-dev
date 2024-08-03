958a10131eb695d18267ab66294470d533f77d47
@dev.arthur <contato.arthurreis222@gmail.com>
2024-08-03 19:01:24 -0300

    feat: add page details inicial

---
+++ src/pages/Details/index.tsx
@@ -0,0 +1,7 @@
+
+
+export const Details = () => {
+  return (
+    <div>Details</div>
+  )
+}
--- src/routes/auth.tsx
+++ src/routes/auth.tsx
@@ -1,11 +1,13 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { Dashboard, Home, New } from '../pages';
+import { Details } from '../pages/Details';
 
 export default () => {
   return (
     <Routes>
       <Route path="/home" element={<Home />} />
       <Route path="/new" element={<New />} />
+      <Route path="/details/:id" element={<Details />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="*" element={<Navigate to="/home" />} />
     </Routes>
