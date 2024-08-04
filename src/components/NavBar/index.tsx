91de8618b62d831fcad24436a5c6f019c757e367
@dev.arthur <contato.arthurreis222@gmail.com>
2024-08-04 19:11:45 -0300

    refactor: navBar

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import { useUser } from '../../context/User';
+import { LINKS } from '../../utils/links';
 
 export const NavBar = () => {
   const { Logout } = useUser();
@@ -7,15 +8,11 @@ export const NavBar = () => {
   return (
     <nav className="w-full flex justify-between items-center rounded-lg px-6 py-3 mb-10 bg-red-600">
       <div className="flex items-center gap-4">
-        <Link to="/home" className="font-bold text-neutral-50">
-          Dashboard
-        </Link>
-        <Link to="/dashboard" className="font-bold text-neutral-50">
-          Minha Garagem
-        </Link>
-        <Link to="/new" className="font-bold text-neutral-50">
-          Cadastrar Carro
-        </Link>
+        {LINKS.map((l) => (
+          <Link to={l.path} className="font-semibold text-neutral-50">
+            {l.title}
+          </Link>
+        ))}
       </div>
       <button className="font-bold text-neutral-50" onClick={Logout}>
         Sair
---
+++ src/utils/links.ts
@@ -0,0 +1,14 @@
+export const LINKS = [
+  {
+    path: '/home',
+    title: 'Dashboard',
+  },
+  {
+    path: '/dashboard',
+    title: 'Minha Garagem',
+  },
+  {
+    path: '/new',
+    title: 'Cadastrar Carro',
+  },
+];
