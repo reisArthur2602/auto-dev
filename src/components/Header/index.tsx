2ee65c0deff537367c91bd4b13c5e49532f5630a
@dev.arthur <contato.arthurreis222@gmail.com>
2024-08-04 17:05:18 -0300

    feat: add link para dashboard

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,20 @@
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/logo.svg';
+import { useUser } from '../../context/User';
 import { Container } from '../Container';
+import { UserCircle2 } from 'lucide-react';
 
 export const Header = () => {
+  const { isAuth } = useUser();
   return (
     <header className="w-full h-20 bg-white shadow-sm flex items-center justify-center">
       <Container classname="flex items-center justify-between">
         <img src={Logo} alt="AutoDev Logo" />
+        {isAuth && (
+          <Link to="/dashboard">
+            <UserCircle2 size={40} color="#0A0A0A" />
+          </Link>
+        )}
       </Container>
     </header>
   );
